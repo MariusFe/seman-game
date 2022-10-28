9c2db7dfb21b16b3c01c0110576c517ee45ff96a
MariusFe <mferrieres@hotmail.com>
2022-10-28 12:03:10 +0200

    changes

--- back.py
+++ back.py
@@ -1,94 +1,100 @@
-from importlib.machinery import WindowsRegistryFinder
 import wikipediaapi
 import requests
 import re
 from gensim.models import KeyedVectors
-import json
 
+class Back:
 
-def getArticle():
-    #Creating the session and preparing the url
+    def __init__(self):
+        self.words = {}
 
-    s= requests.Session()
-    URL = "https://fr.wikipedia.org/w/api.php"
-    taille_article = 1000
-    nombre_paragraphes = 10
+    def getArticle(self):
+        #Creating the session and preparing the url
 
-    PARAMS = {
-        "action": "query",
-        "format": "json",
-        "list": "random"
-    }
+        s= requests.Session()
+        URL = "https://fr.wikipedia.org/w/api.php"
+        taille_article = 1000
+        nombre_paragraphes = 10
 
-    wiki_wiki = wikipediaapi.Wikipedia(
-        language='fr',
-        extract_format=wikipediaapi.ExtractFormat.WIKI
-    )
+        PARAMS = {
+            "action": "query",
+            "format": "json",
+            "list": "random"
+        }
 
-    # We are getting a random article
-    R = s.get(url=URL, params=PARAMS)
-    DATA = R.json()
-    DATA["query"]["random"][0]["title"] = ':'
+        wiki_wiki = wikipediaapi.Wikipedia(
+            language='fr',
+            extract_format=wikipediaapi.ExtractFormat.WIKI
+        )
 
-    # If it contains ':' that means it is a discussion, a user or whatever
-    # We then loop until we find a article without ':'
-    while re.compile(r':').findall(DATA["query"]["random"][0]["title"]):
+        # We are getting a random article
         R = s.get(url=URL, params=PARAMS)
         DATA = R.json()
+        DATA["query"]["random"][0]["title"] = ':'
+
+        # If it contains ':' that means it is a discussion, a user or whatever
+        # We then loop until we find a article without ':'
+        while re.compile(r':').findall(DATA["query"]["random"][0]["title"]):
+            R = s.get(url=URL, params=PARAMS)
+            DATA = R.json()
+            # print(DATA["query"]["random"][0]["title"])
+            page_py = wiki_wiki.page(DATA["query"]["random"][0]["title"])
+            # print(len(page_py.text.split(" ")))
+            # If the article has less than 'taille_article' words we add ':' to the title to loop again
+            if len(page_py.text.split(" ")) < taille_article:
+                DATA["query"]["random"][0]["title"] = DATA["query"]["random"][0]["title"] + ":"
+
+        # print(page_py.categories)
+        # print(page_py.text)
         # print(DATA["query"]["random"][0]["title"])
-        page_py = wiki_wiki.page(DATA["query"]["random"][0]["title"])
-        # print(len(page_py.text.split(" ")))
-        # If the article has less than 'taille_article' words we add ':' to the title to loop again
-        if len(page_py.text.split(" ")) < taille_article:
-            DATA["query"]["random"][0]["title"] = DATA["query"]["random"][0]["title"] + ":"
 
-    # print(page_py.categories)
-    # print(page_py.text)
-    # print(DATA["query"]["random"][0]["title"])
-
-    # We split in paragraphs (may be useful later) and spaces 
-    paragraphes = page_py.text.split("\n")[:nombre_paragraphes]
-    paragraphes = [re.split(r" ", par) for par in paragraphes]
+        # We split in paragraphs (may be useful later) and spaces 
+        paragraphes = page_py.text.split("\n")[:nombre_paragraphes]
+        paragraphes = [re.split(r" ", par) for par in paragraphes]
 
 
-    i=0
+        i=0
 
-    words = {
-        "titre": [],
-        "article": []
-    }
+        self.words = {
+            "words":[]
+        }
 
-    # TO TEST THE STUFF, REMOVE FOR PRODUCTION
-    # titre = "Ceci est un test de titre"
-    # paragraphes = [["Ceci","est","un","test","de","texte"]]
+        # TO TEST THE STUFF, REMOVE FOR PRODUCTION
+        # titre = "Ceci est un test de titre"
+        # paragraphes = [["Ceci","est","un","test","de","texte"]]
 
-    for mot in DATA["query"]["random"][0]["title"].split(" "):
-        words["titre"].append({
-                "id": i,
-                "mot": mot,
-                "trouve": False,
-                "mot_proche": None,
-                "percentage": 0,
-                "character": False
-        })
-        i += 1
-
-    for par in paragraphes:
-        for word in par:
-            words["article"].append({
-                "id": i,
-                "mot": word,
-                "trouve": False,
-                "mot_proche": None,
-                "percentage": 0,
-                "character": False
+        for mot in DATA["query"]["random"][0]["title"].split(" "):
+            self.words["words"].append({
+                    "mot": mot,
+                    "type": "titre",
+                    "character": False
             })
             i += 1
-        words["article"].append({"mot": "\n"})
 
-    return words
+        for par in paragraphes:
+            for word in par:
+                self.words["words"].append({
+                    "mot": word,
+                    "type": "article",
+                    "character": False
+                })
+                i += 1
+            self.words["words"].append({
+                "mot": "\n", 
+                "type": "article",
+                "character": True
+            })
 
-def semantique(mot1, mot2):
+        return self.words
 
-  model = KeyedVectors.load_word2vec_format("./data/model.bin", binary=True, unicode_errors="ignore")
-  return model.distance(mot1, mot2), model.similarity(mot1, mot2)
+    def semantique(self, mot1, mot2):
+
+        try:
+            model = KeyedVectors.load_word2vec_format("./data/model.bin", binary=True, unicode_errors="ignore")
+            return model.distance(mot1, mot2), model.similarity(mot1, mot2)
+        except Exception:
+            return None
+
+    def getWords(self):
+
+        return self.words
--- index.py
+++ index.py
@@ -1,39 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import back
 import json
 
 app = Flask(__name__,static_folder=".",static_url_path='')
-texte = {}
+_back = back.Back()
+send = {}
 
 @app.route('/new_article', methods=['POST'])
 def new_article():
-  texte = back.getArticle()
+  texte = _back.getArticle()
   send = {
-    "titre": [],
-    "article": []
+    "words": []
   }
 
   i = 0
 
-  for mot in texte["titre"]:
-    send["titre"].append({
-      "id": i,
-      "classes": ["titre", "cache"],
-      "mot": "#" * len(mot["mot"]),
-      "percentage": 0
-    })
-    i += 1
-
-  for mot in texte["article"]:
-    send["article"].append({
-      "id": i,
-      "classes": ["article", "cache"],
-      "mot": "#" * len(mot["mot"]),
-      "percentage": 0
-    })
+  for mot in texte["words"]:
+    send["words"].append({
+        "id": i,
+        "mot": "#" * len(mot["mot"]),
+        "type": mot["type"],
+        "etat": ["cache"],
+        "percentage": 0,
+        "character": mot["character"]
+      })
     i += 1
   
   return render_template('index.html', receive=send, receive1 = json.dumps(send))
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+  submit = request.form.get("in_word")
+  result = _back.semantique("oui", submit)
+  words = _back.getWords()
+
+  if result == None:
+    return render_template('index.html', receive = send, receive1 = json.dumps(send))
+
+  i = 0
+  for word in words:
+
+    if submit == word:
+      send["words"][i]["mot"] = word
+      send["words"][i]["classes"] = [part, "trouve"]
+
+    if result[1] > 0.2 and submit != word:
+      send["words"][i]["mot"] = submit
+      send["words"][i]["classes"] = [part, "proche"]
+      send["words"][i]["percentage"] = result[1]
+
+    i += 1
+
+  return render_template('index.html', receive = send, receive1 = json.dumps(send))
 
 @app.route('/')
 def home():
